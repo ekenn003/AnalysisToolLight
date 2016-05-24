54ec3f5d89e6a54da7b9d6c617cd2e3b87bc34f6
E Kennedy <ekennedy@cern.ch>
2016-05-24 10:13:44 +0200

    command line input files

--- VH/python/FinalState_4mu.py
+++ VH/python/FinalState_4mu.py
@@ -1,27 +1,29 @@
 # VH/FinalState_4mu.py
 import glob
 import itertools
+import argparse
 from collections import OrderedDict
 import ROOT
+from AnalysisBase import main as analysisBaseMain
 from AnalysisToolLight.AnalysisToolLight.AnalysisBase import AnalysisBase
 
 ## ___________________________________________________________
 class CutFlow(object):
+    '''
+    The CutFlow object keeps track of the cutflow and helps fill
+    the efficiencies histogram at the end of the job.
+    '''
     def __init__(self, *args):
         self.counters = OrderedDict()
-        for arg in args:
+        for arg in args: # initialise all counters to 0
             self.counters[arg] = 0
-
-    def proliferate(self, arg):
+    def proliferate(self, arg): # increases counter "arg" by 1
         self.counters[arg] += 1.
-
-    def numBins(self):
+    def numBins(self): # returns number of counters
         return len(self.counters)
-
-    def count(self,arg):
-        return self.counters[arg]
-
-    def getNames(self):
+    def count(self, counter): # returns the current value of the counter "arg"
+        return self.counters[counter]
+    def getNames(self): # returns ordered dict of counters
         return self.counters.keys()
 
 
@@ -29,34 +31,46 @@ class CutFlow(object):
 class VH4Mu(AnalysisBase):
     def __init__(self, **kwargs):
         filenames = []
-        with open('VH/data/inputfiles.txt','r') as f:
+        inputFileList = kwargs.pop(inputFileList, [])
+        with open('inputFileList','r') as f:
             for line in f.readlines():
-                filenames += glob.glob(line.strip())
+                self.filenames += glob.glob(line.strip())
         super(VH4Mu, self).__init__(filenames = filenames, **kwargs)
-
-        # event counters
+        #############################
+        # Initialise event counters #
+        #############################
         self.cutflow = CutFlow(
-            "nEv_Skim",
-            "nEv_PV",
+            'nEv_Skim',
+            'nEv_PV',
         )
 
-        # Book histograms
-        self.histograms["hLeadMuPt"] = ROOT.TH1F("hLeadMuPt", "hLeadMuPt", 200, 0, 200)
-        self.histograms["hSubLeadMuPt"] = ROOT.TH1F("hSubLeadMuPt", "hSubLeadMuPt", 200, 0, 200)
-        self.histograms["hDiMuPt"] = ROOT.TH1F("hDiMuPt", "hDiMuPt", 200, 0, 200)
-        self.histograms["hDiMuInvMass"] = ROOT.TH1F("hDiMuInvMass", "hDiMuInvMass", 200, 0, 200)
+        #############################
+        # Book histograms ###########
+        #############################
+        self.histograms['hLeadMuPt']    = ROOT.TH1F('hLeadMuPt', 'hLeadMuPt', 200, 0, 200)
+        self.histograms['hSubLeadMuPt'] = ROOT.TH1F('hSubLeadMuPt', 'hSubLeadMuPt', 200, 0, 200)
+        self.histograms['hDiMuPt']      = ROOT.TH1F('hDiMuPt', 'hDiMuPt', 200, 0, 200)
+        self.histograms['hDiMuInvMass'] = ROOT.TH1F('hDiMuInvMass', 'hDiMuInvMass', 200, 0, 200)
 
-        self.histograms["hEfficiencies"] = ROOT.TH1F("hEfficiencies", "hEfficiencies", self.cutflow.numBins(), 0, self.cutflow.numBins())
+        self.histograms['hEfficiencies'] = ROOT.TH1F('hEfficiencies', 'hEfficiencies', self.cutflow.numBins(), 0, self.cutflow.numBins())
 
-## ___________________________________________________________
+    ## _______________________________________________________
     def perEventAction(self):
-        # cuts
+        '''
+        This is the core of the analysis loop. Selection is done here.
+        '''
+        #############################
+        # Define cuts ###############
+        #############################
         cMuPt = 20.
         cMuEta = 2.4
         cDiMuPt = 50.
 
-        self.cutflow.proliferate("nEv_Skim")
+        self.cutflow.proliferate('nEv_Skim')
 
+        #############################
+        # MUONS #####################
+        #############################
         # loop over muons and save the good ones
         self.goodMuons = []
         for muon in self.muons:
@@ -70,6 +84,9 @@ class VH4Mu(AnalysisBase):
 
 
 
+        #############################
+        # DIMUON PAIRS ##############
+        #############################
         # loop over all possible pairs of muons
         self.dimuon = () 
         maxDiMuPt = 0
@@ -86,32 +103,44 @@ class VH4Mu(AnalysisBase):
         self.fill()
 
 
-## ___________________________________________________________
+    ## _______________________________________________________
     def fill(self):
+        '''
+        At the end of the analyze loop, fill the histograms. Called once per event.
+        '''
         weight = 1.
-        # lead muon
+        # leading muon
         mu0 = self.dimuon[0]
-        self.fillVar("hLeadMuPt", mu0.Pt(), weight)
-        # lead muon
+
+        self.histograms['hLeadMuPt'].Fill(mu0.Pt(), weight)
+
+        # subleading muon
         mu1 = self.dimuon[1]
-        self.fillVar("hSubLeadMuPt", mu1.Pt(), weight)
+        self.histograms['hSubLeadMuPt'].Fill(mu1.Pt(), weight)
+
         # diumuon
         diMuP4 = mu0.P4() + mu1.P4()
-        self.fillVar("hDiMuPt", diMuP4.Pt(), weight)
-        self.fillVar("hDiMuInvMass", diMuP4.M(), weight)
+        self.fillVar('hDiMuPt', diMuP4.Pt(), weight)
+        self.fillVar('hDiMuInvMass', diMuP4.M(), weight)
         
 
-## ___________________________________________________________
-    def fillVar(self, histName, val, weight=1.):
-        self.histograms[histName].Fill(val, weight)
 
-## ___________________________________________________________
+    ## _______________________________________________________
     def endJob(self):
+        '''
+        At the end of the job, fill efficiencies histogram.
+        '''
         for i, name in enumerate(self.cutflow.getNames()):
             # 0 is the underflow bin in root: first bin to fill is bin 1
-            self.histograms["hEfficiencies"].SetBinContent(i+1, self.cutflow.count(name))
-            self.histograms["hEfficiencies"].GetXaxis().SetBinLabel(i+1, name)
+            self.histograms['hEfficiencies'].SetBinContent(i+1, self.cutflow.count(name))
+            self.histograms['hEfficiencies'].GetXaxis().SetBinLabel(i+1, name)
 
+## ___________________________________________________________
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
 
-
-VH4Mu().analyze()
+# actually execute the analysis
+def main(argv=None):
+    args = analysisBaseMain(argv)
+    VH4Mu(inputFileList = args.inputFileList).analyze()
