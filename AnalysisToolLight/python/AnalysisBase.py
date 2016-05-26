36d152d96f3227c1749aec8c27a8c471da96fc1b
E Kennedy <ekennedy@cern.ch>
2016-05-26 15:21:32 +0200

    vertices and muon iso

--- AnalysisToolLight/python/AnalysisBase.py
+++ AnalysisToolLight/python/AnalysisBase.py
@@ -4,6 +4,7 @@ import glob
 import os
 import sys
 import ROOT
+from prettytable import PrettyTable
 from Dataform import *
 
 ## ___________________________________________________________
@@ -13,6 +14,12 @@ class AnalysisBase(object):
     '''
     ## _______________________________________________________
     def __init__(self, args):
+        # set up logging info
+        logging.getLogger('Analysis')
+        #logging.basicConfig(level=logging.INFO, stream=sys.stderr, format='%(asctime)s: %(message)s', datefmt='%H:%M:%S')
+        logging.basicConfig(level=logging.INFO, stream=sys.stderr, format='[%(asctime)s]   %(message)s', datefmt='%Y-%m-%d %H:%M')
+
+
         # set defaults. These can be overridden with command line arguments (after adding to Base)
         self.filenames = []
         self.treedir   = 'makeroottree'
@@ -27,25 +34,34 @@ class AnalysisBase(object):
             for line in f.readlines():
                 self.filenames += glob.glob(line.strip())
 
+        lumichain = ROOT.TChain('{0}/{1}'.format(self.treedir, self.luminame))
 
-
-
-        self.sumweight = 0
-        self.nevents   = 0
+        self.sumweights = 0
+        self.numlumis   = 0
+        self.nevents    = 0
         # things we will check in the first file
         self.isdata = True
         # get the summed weights of processed entries (add up all lumi_sumweights in AC1Blumi)
         for f, fname in enumerate(self.filenames):
             tfile = ROOT.TFile(fname)
-            print 'Adding file ' + str(f+1) + ': ' + fname
+            logging.info('Adding file {0}: {1}'.format(f+1, fname))
             lumitree = tfile.Get('{0}/{1}'.format(self.treedir, self.luminame))
             infotree = tfile.Get('{0}/{1}'.format(self.treedir, self.infoname))
-            self.sumweight += lumitree.lumi_sumweights
-            self.nevents += infotree.nevents
             self.isdata = infotree.isdata
+            tfile.Close('R')
+            lumichain.Add(fname)
+
+        logging.info('Assembling lumi information...')
+        # iterate over lumis to find total number of events
+        self.numlumis = lumichain.GetEntries()
+        for entry in xrange(self.numlumis):
+            lumichain.GetEntry(entry)
+            self.sumweights += lumichain.lumi_sumweights
+            self.nevents    += lumichain.lumi_nevents
 
 
-        print 'Number of events found: ' + str(self.nevents)
+        logging.info('Number of events found: {0} in {1} lumi sections in {2} files'.format(self.nevents, self.numlumis, len(self.filenames)))
+        logging.info('Sample will be processed as {0}'.format('DATA' if self.isdata else 'MC'))
 
         # initialize output file
         self.outfile = ROOT.TFile(self.output,'RECREATE')
@@ -53,7 +69,6 @@ class AnalysisBase(object):
         # initialize list of histograms as empty
         self.histograms = {}
 
-        print 
 
     ## _______________________________________________________
     def analyze(self):
@@ -65,14 +80,18 @@ class AnalysisBase(object):
         eventsprocessed = 0
         # Iterate over each input file
         for f, fname in enumerate(self.filenames):
+            logging.info('Processing file {0} of {1}:'.format(f+1, len(self.filenames)))
+
             tfile = ROOT.TFile(fname)
             tree = tfile.Get('{0}/{1}'.format(self.treedir,self.treename))
+
             # Iterate over each event
             for row in tree:
                 eventsprocessed += 1
-                if eventsprocessed%1000==0: print 'Processing event ' + str(eventsprocessed) + '/' + str(self.nevents)
+                if eventsprocessed%1000==0: logging.info('  Processing event {0}/{1}'.format(eventsprocessed, self.nevents))
                 # Load collections
-                #self.vertices  = [Vertex(row, i) for i in range(row.primvertex_count)]
+                self.event     = Event(row)
+                self.vertices  = [Vertex(row, i) for i in range(row.primvertex_count)]
                 self.muons     = [Muon(row, i) for i in range(row.muon_count)]
                 self.electrons = [Electron(row, i) for i in range(row.electron_count)]
                 self.taus      = [Tau(row, i) for i in range(row.tau_count)]
@@ -91,7 +110,7 @@ class AnalysisBase(object):
         Each physics object is available via:
                self.muons
                self.electrons
-               ...
+               etc.
         '''
         pass
 
@@ -99,19 +118,51 @@ class AnalysisBase(object):
 
     ## _______________________________________________________
     def endJob(self):
-        ''' 
-        Dummy function overridden in derived class
         '''
-        pass
+        At the end of the job, fill efficiencies histogram.
+        '''
+        # initialise cutflow histogram
+        self.histograms['hEfficiencies'] = ROOT.TH1F('hEfficiencies', 'hEfficiencies', self.cutflow.numBins(), 0, self.cutflow.numBins())
+        self.histograms['hEfficiencies'].GetXaxis().SetTitle('')
+        self.histograms['hEfficiencies'].GetYaxis().SetTitle('')
+        # fill histogram
+        for i, name in enumerate(self.cutflow.getNames()):
+            # 0 is the underflow bin in root: first bin to fill is bin 1
+            self.histograms['hEfficiencies'].SetBinContent(i+1, self.cutflow.count(name))
+            self.histograms['hEfficiencies'].GetXaxis().SetBinLabel(i+1, name)
+
+        # print cutflow table
+        logging.info('Job complete.')
+        logging.info('NEVENTS: {0}'.format(self.nevents))
+        logging.info('SUMWEIGHTS: {0}'.format(self.sumweights))
+
+        efftable = PrettyTable(['Selection', 'Events', 'Eff.(Skim) [%]', 'Rel.Eff. [%]'])
+        efftable.align['Selection'] = 'l'
+        efftable.align['Events'] = 'r'
+        efftable.align['Eff.(Skim) [%]'] = 'r'
+        efftable.align['Rel.Eff. [%]'] = 'r'
+        skimnevents = self.cutflow.count(self.cutflow.getNames()[0])
+
+        for i, name in enumerate(self.cutflow.getNames()):
+            efftable.add_row([self.cutflow.getPretty(name), format(self.cutflow.count(name), '0.0f'), self.cutflow.getSkimEff(i), self.cutflow.getRelEff(i)])
+
+
+         
+        logging.info('Cutflow summary:\n\n' + efftable.get_string() + '\n')
+
+
+
 
     ## _______________________________________________________
     def write(self):
+        '''
+        Writes the histograms to the output file and saves it.
+        '''
         self.outfile.cd()
         for hist in self.histograms:
             self.histograms[hist].Write()
-
+        logging.info('Output file ' + self.output + ' created.')
         self.outfile.Close()
-
 
 
 ## ___________________________________________________________
@@ -120,8 +171,9 @@ def parse_command_line(argv):
 
     # line below is an example of a required argument
     parser.add_argument('inputFileList', type=str, help='List of input files (AC1B*.root)')
+    parser.add_argument('outputFileName', type=str, help='Output file name')
     # line below is an example of an optional argument
-    parser.add_argument('--outputFileName', type=str, default='ana.out', help='Output file name')
+    #parser.add_argument('--outputFileName', type=str, default='ana.out', help='Output file name')
 
     return parser.parse_args(argv)
 
--- VH/python/FinalState_4mu.py
+++ VH/python/FinalState_4mu.py
@@ -5,50 +5,147 @@ import argparse
 import sys
 import ROOT
 from collections import OrderedDict
+from AnalysisToolLight.AnalysisToolLight.AnalysisBase import AnalysisBase, CutFlow
 from AnalysisToolLight.AnalysisToolLight.AnalysisBase import main as analysisBaseMain
-from AnalysisToolLight.AnalysisToolLight.AnalysisBase import AnalysisBase
-
-## ___________________________________________________________
-class CutFlow(object):
-    '''
-    The CutFlow object keeps track of the cutflow and helps fill
-    the efficiencies histogram at the end of the job.
-    '''
-    def __init__(self, *args):
-        self.counters = OrderedDict()
-        for arg in args: # initialise all counters to 0
-            self.counters[arg] = 0
-    def proliferate(self, arg): # increases counter "arg" by 1
-        self.counters[arg] += 1.
-    def numBins(self): # returns number of counters
-        return len(self.counters)
-    def count(self, counter): # returns the current value of the counter "arg"
-        return self.counters[counter]
-    def getNames(self): # returns ordered dict of counters
-        return self.counters.keys()
-
 
 ## ___________________________________________________________
 class VH4Mu(AnalysisBase):
     def __init__(self, args):
         super(VH4Mu, self).__init__(args)
         #############################
+        # Define cuts ###############
+        #############################
+        # PV cuts
+        self.cVtxNdf = 4
+        self.cVtxZ   = 24. # cm
+
+        # muon cuts
+        self.cMuPt = 10. # GeV
+        self.cMuEta = 2.4
+        self.cMuPtMax = 20. # choice here should depend on HLT
+        self.cMuEtaMax = 2.4 # choice here should depend on HLT
+        # muon pv cuts
+        self.cDxyMu = 0.02 # cm
+        self.cDzMu  = 0.14 # cm
+
+        # isolation (https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideMuonIdRun2#Muon_Isolation)
+        self.cIsoMuType = 'PF_dB' # PF combined w/dB correction Loose
+        #self.cIsoMuType = 'tracker' # Tracker-based
+        self.cIsoMuLevel = 'tight'
+        #self.cIsoMuLevel = 'loose'
+
+        # muon id 
+        #self.cMuID = 'tight'
+        self.cMuID = 'medium'
+        #self.cMuID = 'loose'
+
+        # dimuon pair cuts
+        self.cDiMuInvMass = 50. # GeV
+        self.cDiMuPt = 38. # GeV
+
+        # electron cuts
+        self.cPtE = 10.
+        self.cEtaE = 2.4
+
+        # jet cuts
+        self.cPtJet = 30. # GeV
+        self.cEtaJet = 4.7
+
+        # MET cuts
+        self.cMET = 40. # GeV
+
+
+
+
+
+        #############################
         # Initialise event counters #
         #############################
-        self.cutflow = CutFlow(
-            'nEv_Skim',
-            'nEv_PV',
-        )
+        self.cutflow = CutFlow()
+        self.cutflow.add('nEv_Skim', 'Skim number of events')
+        # event selection
+        self.cutflow.add('nEv_Trigger', 'Trigger')
+        self.cutflow.add('nEv_PV', 'PV cuts')
+        # muon selection
+        self.cutflow.add('nEv_GAndTr',   'Global+Tracker muon')
+        self.cutflow.add('nEv_Pt',       'Muon pT > {0}'.format(self.cMuPt))
+        self.cutflow.add('nEv_Eta',      'Muon eta < {0}'.format(self.cMuEta))
+        self.cutflow.add('nEv_PtEtaMax', 'At least 1 trigger-matched mu with pT > {0} and |eta| < {1}'.format(self.cMuPtMax, self.cMuEtaMax))
+        self.cutflow.add('nEv_Iso',      'Muon has {0} {1} isolation'.format(self.cIsoMuType, self.cIsoMuLevel))
+        self.cutflow.add('nEv_ID',       'Muon has {0} muon ID'.format(self.cMuID))
+        self.cutflow.add('nEv_PVMu',     'Muon Dxy < {0} and Dx < {1}'.format(self.cMuDxy, self.cMuDz))
+        # muon pair slection
+        self.cutflow.add('nEv_SamePVDiMu',  'Dimu pair has same pv mus')
+        self.cutflow.add('nEv_ChargeDiMu',  'Dimu pair has opposite-sign mus')
+        self.cutflow.add('nEv_InvMassDiMu', 'Dimu pair has invariant mass > {0}'.format(self.cDiMuInvMass))
+        self.cutflow.add('nEv_PtDiMu',      'Dimu pair has pT > {0}'.format(self.cDiMuPt))
+
+
+
+
+
 
         #############################
         # Book histograms ###########
         #############################
-        self.histograms['hLeadMuPt']    = ROOT.TH1F('hLeadMuPt', 'hLeadMuPt', 200, 0, 200)
-        self.histograms['hSubLeadMuPt'] = ROOT.TH1F('hSubLeadMuPt', 'hSubLeadMuPt', 200, 0, 200)
-        self.histograms['hDiMuPt']      = ROOT.TH1F('hDiMuPt', 'hDiMuPt', 200, 0, 200)
-        self.histograms['hDiMuInvMass'] = ROOT.TH1F('hDiMuInvMass', 'hDiMuInvMass', 200, 0, 200)
+        self.histograms['hVtxN'] = ROOT.TH1F('hVtxN', 'hVtxN', 100, 0., 100.)
+        self.histograms['hVtxN'].GetXaxis().SetTitle('N_{PV}')
+        self.histograms['hVtxN'].GetYaxis().SetTitle('Candidates')
+        self.histograms['hVtxN_u'] = ROOT.TH1F('hVtxN_u', 'hVtxN_u', 100, 0., 100.)
+        self.histograms['hVtxN_u'].GetXaxis().SetTitle('N_{PV} before weighting')
+        self.histograms['hVtxN_u'].GetYaxis().SetTitle('Candidates')
 
-        self.histograms['hEfficiencies'] = ROOT.TH1F('hEfficiencies', 'hEfficiencies', self.cutflow.numBins(), 0, self.cutflow.numBins())
+        self.histograms['hWeight'] = ROOT.TH1F('hWeight', 'hWeight', 100000, -10000., 10000.)
+        self.histograms['hWeight'].GetXaxis().SetTitle('Event weight')
+        self.histograms['hWeight'].GetYaxis().SetTitle('Events')
+
+        # muons
+        self.histograms['hMuPt'] = ROOT.TH1F('hMuPt', 'hMuPt', 500, 0., 1000.)
+        self.histograms['hMuPt'].GetXaxis().SetTitle('p_{T #mu}[GeV/c]')
+        self.histograms['hMuPt'].GetYaxis().SetTitle('Candidates/2.0[GeV/c]')
+        self.histograms['hMuEta'] = ROOT.TH1F('hMuEta', 'hMuEta',  52, -2.6, 2.6)
+        self.histograms['hMuEta'].GetXaxis().SetTitle('#eta_{#mu}')
+        self.histograms['hMuEta'].GetYaxis().SetTitle('Candidates/0.1')
+        self.histograms['hMuPhi'] = ROOT.TH1F('hMuPhi', 'hMuPhi', 34, -3.4, 3.4)
+        self.histograms['hMuPhi'].GetXaxis().SetTitle('#varphi_{#mu} [rad]')
+        self.histograms['hMuPhi'].GetYaxis().SetTitle('Candidates/0.2[rad]')
+
+        # leading/subleading good muons
+        self.histograms['hLeadMuPt'] = ROOT.TH1F('hLeadMuPt', 'hLeadMuPt', 500, 0., 1000.)
+        self.histograms['hLeadMuPt'].GetXaxis().SetTitle('p_{T #mu}[GeV/c]')
+        self.histograms['hLeadMuPt'].GetYaxis().SetTitle('Candidates/2.0[GeV]')
+        self.histograms['hSubLeadMuPt'] = ROOT.TH1F('hSubLeadMuPt', 'hSubLeadMuPt', 500, 0., 1000.)
+        self.histograms['hSubLeadMuPt'].GetXaxis().SetTitle('p_{T #mu}[GeV/c]')
+        self.histograms['hSubLeadMuPt'].GetYaxis().SetTitle('Candidates/2.0[GeV]')
+
+        # dimuon
+        self.histograms['hDiMuPt'] = ROOT.TH1F('hDiMuPt', 'hDiMuPt', 500, 0., 1000.)
+        self.histograms['hDiMuPt'].GetXaxis().SetTitle('p_{T #mu^{+}#mu^{-}}[GeV/c]')
+        self.histograms['hDiMuPt'].GetYaxis().SetTitle('Candidates/2.0[GeV]')
+        self.histograms['hDiMuEta'] = ROOT.TH1F('hDiMuEta', 'hDiMuEta',  132, -6.6, 6.6)
+        self.histograms['hDiMuEta'].GetXaxis().SetTitle('#eta_{#mu^{+}#mu^{-}}')
+        self.histograms['hDiMuEta'].GetYaxis().SetTitle('Candidates/0.1')
+        self.histograms['hDiMuPhi'] = ROOT.TH1F('hDiMuPhi', 'hDiMuPhi', 34, -3.4, 3.4)
+        self.histograms['hDiMuPhi'].GetXaxis().SetTitle('#varphi_{#mu^{+}#mu^{-}} [rad]')
+        self.histograms['hDiMuPhi'].GetYaxis().SetTitle('Candidates/0.2[rad]')
+
+        self.histograms['hDiMuDeltaPt'] = ROOT.TH1F('hDiMuDeltaPt', 'hDiMuDeltaPt', 320, -800., 800.)
+        self.histograms['hDiMuDeltaPt'].GetXaxis().SetTitle('#Delta p_{T #mu^{+} - #mu^{-}}[GeV/c]')
+        self.histograms['hDiMuDeltaPt'].GetYaxis().SetTitle('Candidates/5.0[GeV]')
+        self.histograms['hDiMuDeltaEta'] = ROOT.TH1F('hDiMuDeltaEta', 'hDiMuDeltaEta',  132, -6.6, 6.6)
+        self.histograms['hDiMuDeltaEta'].GetXaxis().SetTitle('#Delta #eta_{#mu^{+} - #mu^{-}}')
+        self.histograms['hDiMuDeltaEta'].GetYaxis().SetTitle('Candidates/0.1')
+        self.histograms['hDiMuDeltaPhi'] = ROOT.TH1F('hDiMuDeltaPhi', 'hDiMuDeltaPhi', 34, -3.4, 3.4)
+        self.histograms['hDiMuDeltaPhi'].GetXaxis().SetTitle('#Delta #varphi_{#mu^{+} - #mu^{-}} [rad]')
+        self.histograms['hDiMuDeltaPhi'].GetYaxis().SetTitle('Candidates/0.2[rad]')
+
+        self.histograms['hDiMuInvMass'] = ROOT.TH1F('hDiMuInvMass', 'hDiMuInvMass', 2000, 0, 1000)
+        self.histograms['hDiMuInvMass'].GetXaxis().SetTitle('M_{#mu^{+}#mu^{-}} [GeV/c^{2}]')
+        self.histograms['hDiMuInvMass'].GetYaxis().SetTitle('Candidates/0.5[GeV/c^{2}]')
+
+
+
+
 
     ## _______________________________________________________
     def perEventAction(self):
@@ -56,28 +153,118 @@ class VH4Mu(AnalysisBase):
         This is the core of the analysis loop. Selection is done here.
         '''
         #############################
-        # Define cuts ###############
+        # Event weight ##############
         #############################
-        cMuPt = 20.
-        cMuEta = 2.4
-        cDiMuPt = 50.
+        pileupweight = 1.
+        if not self.isdata: pileupweight = self.event.GenWeight()
+        self.histograms['hWeight'].Fill(pileupweight)
 
-        self.cutflow.proliferate('nEv_Skim')
+        self.cutflow.increment('nEv_Skim')
+
+
+        #############################
+        # Trigger ###################
+        #############################
+        # debugging:
+        evtnr = self.event.Number()
+        if not evtnr%2: self.cutflow.increment('nEv_Trigger')
+        else: return
+
+
+
+
+
+        #############################
+        # Primary vertices ##########
+        #############################
+        # save good vertices
+        goodVertices = []
+        isVtxNdfOK = False
+        isVtxZOK = False
+        for pv in self.vertices:
+            if not isVtxNdfOK: isVtxNdfOK = pv.Ndof() > self.cVtxNdf
+            if not isVtxZOK:   isVtxZOK = pv.Z() < self.cVtxZ
+            # check if it's passed
+            if not (isVtxNdfOK and isVtxZOK): continue
+            # save it if it did
+            goodVertices += [pv]
+
+        # require at least one good vertex
+        if not goodVertices: return
+        if (isVtxNdfOK and isVtxZOK): self.cutflow.increment('nEv_PV')
+
+        # fill histograms with good pvs
+        self.histograms['hVtxN'].Fill(len(goodVertices), pileupweight)
+        self.histograms['hVtxN_u'].Fill(len(goodVertices))
+
+
+
+
+
+
+
 
         #############################
         # MUONS #####################
         #############################
         # loop over muons and save the good ones
-        self.goodMuons = []
+        goodMuons = []
+        isGAndTr = False
+        isPtCutOK = False
+        isEtaCutOK = False
+        nMuPtEtaMax = 0
+        isIsoOK = False
+        isIDOK = False
+        isTrackCutOK = False
         for muon in self.muons:
 
             # muon cuts
-            if not muon.Pt() > cMuPt: continue
-            if not muon.Eta() < cMuEta: continue
+            if not (muon.IsGlobal() and muon.IsTracker()): continue
+            isGAndTr = True
+            if not muon.Pt() > self.cMuPt: continue
+            isPtCutOK = True
+            if not muon.Eta() < self.cMuEta: continue
+            isEtaCutOK = True
+
+
+            # check isolation
+            # here you can also do muon.IsoR3CombinedRelIso() < stuff, muon.PFR4ChargedHadrons() etc.
+            # see Muon object in Dataform.py for all avaiable methods
+            if not (muon.CheckIso(cIsoMuType, cIsoMuLevel)): continue
+            isIsoOK = True
+            if not (): continue
+            isIDOK = True
+
+
+
+
+
+            if not (muon.Dxy() < self.cMuDxy and muon.Dz() < self.cMuDz): continue
+            isTrackCutOK = True
+
 
             # if we get to this point, push muon into goodMuons
-            self.goodMuons += [muon]
+            goodMuons += [muon]
 
+
+
+        if isGAndTr: self.cutflow.increment('nEv_GAndTr')
+        if isPtCutOK: self.cutflow.increment('nEv_Pt')
+        if isEtaCutOK: self.cutflow.increment('nEv_Eta')
+        # make sure at least one HLT-matched muon passed extra cuts
+        if nMuPtEtaMax > 0: self.cutflow.increment('nEv_PtEtaMax')
+        else: return
+        if isIsoOK: self.cutflow.increment('nEv_Iso')
+        if isIDOK: self.cutflow.increment('nEv_ID')
+        if isTrackCutOK: self.cutflow.increment('nEv_PVMu')
+
+
+
+
+
+
+        #if self.taus: print 'Tau(0) has pT = ' + str(self.taus[0].Pt())
+        #if self.taus: print ' = ' + str(self.taus[0].TauDiscriminator('cantfindme'))
 
 
         #############################
@@ -86,51 +273,62 @@ class VH4Mu(AnalysisBase):
         # loop over all possible pairs of muons
         self.dimuon = () 
         maxDiMuPt = 0
-        if len(self.goodMuons) >= 2:
-            for pair in itertools.combinations(self.goodMuons,2):
+        if len(goodMuons) >= 2:
+            for pair in itertools.combinations(goodMuons, 2):
                 diMuP4 = pair[0].P4() + pair[1].P4()
-                if diMuP4.Pt() > min(maxDiMuPt, cDiMuPt):
+                if diMuP4.Pt() > min(maxDiMuPt, self.cDiMuPt):
                     maxDiMuPt = diMuP4.Pt()
                     # order the pair by pt
                     self.dimuon = pair if pair[0].Pt() > pair[1].Pt() else (pair[1], pair[0])
 
+
+
+
+
+
+        self.cutflow.add('nEv_SamePVDiMu',  'Dimu pair has same pv mus')
+        self.cutflow.add('nEv_ChargeDiMu',  'Dimu pair has opposite-sign mus')
+        self.cutflow.add('nEv_InvMassDiMu', 'Dimu pair has invariant mass > {0}'.format(self.cDiMuInvMass))
+        self.cutflow.add('nEv_PtDiMu',      'Dimu pair has pT > {0}'.format(self.cDiMuPt))
+
+
+
+
+
+
+
+
+
+
         # fill histograms
-        self.fill()
-
-
-    ## _______________________________________________________
-    def fill(self):
-        '''
-        At the end of the analyze loop, fill the histograms. Called once per event.
-        '''
-        weight = 1.
+        #self.fill()
         # leading muon
-        if len(self.goodMuons) > 0:
-            mu0 = self.goodMuons[0]
-            self.histograms['hLeadMuPt'].Fill(mu0.Pt(), weight)
+        if len(goodMuons) > 0:
+            mu0 = goodMuons[0]
+            self.histograms['hLeadMuPt'].Fill(mu0.Pt(), pileupweight)
 
         # subleading muon
-        if len(self.goodMuons) > 1:
-            mu1 = self.goodMuons[1]
-            self.histograms['hSubLeadMuPt'].Fill(mu1.Pt(), weight)
+        if len(goodMuons) > 1:
+            mu1 = goodMuons[1]
+            self.histograms['hSubLeadMuPt'].Fill(mu1.Pt(), pileupweight)
 
         # diumuon
         if self.dimuon:
             diMuP4 = mu0.P4() + mu1.P4()
-            self.histograms['hDiMuPt'].Fill(diMuP4.Pt(), weight)
-            self.histograms['hDiMuInvMass'].Fill(diMuP4.M(), weight)
+            self.histograms['hDiMuPt'].Fill(diMuP4.Pt(), pileupweight)
+            self.histograms['hDiMuInvMass'].Fill(diMuP4.M(), pileupweight)
         
 
 
-    ## _______________________________________________________
-    def endJob(self):
-        '''
-        At the end of the job, fill efficiencies histogram.
-        '''
-        for i, name in enumerate(self.cutflow.getNames()):
-            # 0 is the underflow bin in root: first bin to fill is bin 1
-            self.histograms['hEfficiencies'].SetBinContent(i+1, self.cutflow.count(name))
-            self.histograms['hEfficiencies'].GetXaxis().SetBinLabel(i+1, name)
+    ### _______________________________________________________
+    #def endJob(self):
+    #    '''
+    #    At the end of the job, fill efficiencies histogram.
+    #    '''
+    #    for i, name in enumerate(self.cutflow.getNames()):
+    #        # 0 is the underflow bin in root: first bin to fill is bin 1
+    #        self.histograms['hEfficiencies'].SetBinContent(i+1, self.cutflow.count(name))
+    #        self.histograms['hEfficiencies'].GetXaxis().SetBinLabel(i+1, name)
 
 
 ## ___________________________________________________________
