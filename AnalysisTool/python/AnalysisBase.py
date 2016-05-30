fbbf4c77ed4d7ca3236863c5224975dfba2e3860
E Kennedy <ekennedy@cern.ch>
2016-05-30 10:38:17 +0200

    pu reweighting

--- AnalysisTool/python/AnalysisBase.py
+++ AnalysisTool/python/AnalysisBase.py
@@ -1,8 +1,7 @@
 import logging
 import argparse
 import glob
-import os
-import sys
+import os, sys
 import ROOT
 from prettytable import PrettyTable
 from Dataform import *
@@ -22,52 +21,92 @@ class AnalysisBase(object):
 
         # set defaults. These can be overridden with command line arguments (after adding to Base)
         self.filenames = []
-        self.treedir   = 'makeroottree'
-        self.treename  = 'AC1B'
-        self.luminame  = 'AC1Blumi'
-        self.infoname  = 'AC1Binfo'
-
-        self.output    = args.outputFileName
-        # put file names into a list called self.filenames
+        self.treedir  = 'makeroottree'
+        self.infoname = 'AC1Binfo'
+        self.luminame = 'AC1Blumi'
+        self.treename = 'AC1B'
+        self.output = args.outputFileName
         inputFileList = args.inputFileList
+        pileupDir = args.pileupDir
+
+
+        # put file names into a list called self.filenames
         with open(inputFileList,'r') as f:
             for line in f.readlines():
                 self.filenames += glob.glob(line.strip())
 
-        lumichain = ROOT.TChain('{0}/{1}'.format(self.treedir, self.luminame))
 
-        self.sumweights = 0
-        self.numlumis   = 0
-        self.nevents    = 0
+        logging.info('Assembling job information...')
         # things we will check in the first file
-        self.isdata = True
-        # get the summed weights of processed entries (add up all lumi_sumweights in AC1Blumi)
-        for f, fname in enumerate(self.filenames):
-            tfile = ROOT.TFile(fname)
-            logging.info('Adding file {0}: {1}'.format(f+1, fname))
-            lumitree = tfile.Get('{0}/{1}'.format(self.treedir, self.luminame))
-            infotree = tfile.Get('{0}/{1}'.format(self.treedir, self.infoname))
-            self.isdata = infotree.isdata
-            tfile.Close('R')
-            lumichain.Add(fname)
+        cmsswversion = ''
+        self.isdata = None
+        # open first file and load info tree
+        tfile0 = ROOT.TFile(self.filenames[0])
+        infotree = tfile0.Get('{0}/{1}'.format(self.treedir, self.infoname))
+        self.isdata = bool(infotree.isdata)
+        #cmsswversion = infotree.CMSSW_version
+        #reading infotree.CMSSW_version is broken right now
+        # hardcode as 76 for now
+        cmsswversion = 'CMSSW_7_6_X'
+        tfile0.Close('R')
 
-        logging.info('Assembling lumi information...')
-        # iterate over lumis to find total number of events
+
+
+        # set up lumi info and see how many events we have to process
+        lumichain = ROOT.TChain('{0}/{1}'.format(self.treedir, self.luminame))
+        self.numlumis   = 0
+        self.sumweights = 0
+        self.nevents    = 0
+        for f, fname in enumerate(self.filenames):
+            logging.info('Adding file {0}: {1}'.format(f+1, fname))
+            lumichain.Add(fname)
+        # iterate over lumis to find total number of events and summed event weights
         self.numlumis = lumichain.GetEntries()
         for entry in xrange(self.numlumis):
             lumichain.GetEntry(entry)
+            self.nevents += lumichain.lumi_nevents
             self.sumweights += lumichain.lumi_sumweights
-            self.nevents    += lumichain.lumi_nevents
 
 
         logging.info('Number of events found: {0} in {1} lumi sections in {2} files'.format(self.nevents, self.numlumis, len(self.filenames)))
         logging.info('Sample will be processed as {0}'.format('DATA' if self.isdata else 'MC'))
 
+
+        # load pileup info and other scale factors
+        logging.info('Loading pileup info...')
+
+        # set up pileup reweighting
+        self.pileupScale = []
+        self.pileupScale_up = []
+        self.pileupScale_down = []
+
+        # get short CMSSW version that was used to produce these
+        version = '{0}{1}X'.format(cmsswversion.split('_')[1], cmsswversion.split('_')[2])
+
+        # now we look for a file called pileup_76X.root or pileup_80X.root in the pileupDir
+        pufile = ROOT.TFile('{0}/pileup_{1}.root'.format(pileupDir, version))
+        logging.info('  Looking for pileup file at {0}/pileup_{1}.root'.format(pileupDir, version))
+
+        # save scale factors in vectors where each index corresponds to the NumTruePileupInteractions
+        scalehist_ = pufile.Get('pileup_scale')
+        for b in range(scalehist_.GetNbinsX()):
+            # set (b)th entry in self.pileupScale to bin content of (b+1)th bin
+            # (0th bin is underflow)
+            self.pileupScale.append(scalehist_.GetBinContent(b+1))
+        scalehist_ = pufile.Get('pileup_scale_up')
+        for b in range(scalehist_.GetNbinsX()):
+            self.pileupScale_up.append(scalehist_.GetBinContent(b+1))
+        scalehist_ = pufile.Get('pileup_scale_down')
+        for b in range(scalehist_.GetNbinsX()):
+            self.pileupScale_down.append(scalehist_.GetBinContent(b+1))
+        pufile.Close()
+
+
+        # initialize map of histograms as empty
+        self.histograms = {}
+
         # initialize output file
         self.outfile = ROOT.TFile(self.output,'RECREATE')
-
-        # initialize list of histograms as empty
-        self.histograms = {}
 
 
     ## _______________________________________________________
@@ -77,6 +116,7 @@ class AnalysisBase(object):
         Iterate over each input file and row in trees.
         Calls the perEventAction method (which is overridden in the derived class).
         '''
+
         eventsprocessed = 0
         # loop over each input file
         for f, fname in enumerate(self.filenames):
@@ -90,14 +130,14 @@ class AnalysisBase(object):
                 eventsprocessed += 1
                 if eventsprocessed%1000==0: logging.info('  Processing event {0}/{1}'.format(eventsprocessed, self.nevents))
                 # load collections
-                self.event     = Event(row)
+                self.event     = Event(row, self.sumweights)
                 self.vertices  = [Vertex(row, i) for i in range(row.primvertex_count)]
                 self.muons     = [Muon(row, i) for i in range(row.muon_count)]
                 self.electrons = [Electron(row, i) for i in range(row.electron_count)]
                 self.photons   = [Photon(row, i) for i in range(row.photon_count)]
                 self.taus      = [Tau(row, i) for i in range(row.tau_count)]
                 self.jets      = [Jet(row, i) for i in range(row.ak4pfchsjet_count)]
-                self.met       = [PFMETTYPE1(row, 0)] # MET is a vector of size 1
+                self.met       = PFMETTYPE1(row, 0) # MET is a vector of size 1
 
                 # do the event analysis!
                 # call the perEventAction method (which is overridden in the derived class)
@@ -142,10 +182,8 @@ class AnalysisBase(object):
         logging.info('SUMWEIGHTS: {0}'.format(self.sumweights))
 
         efftable = PrettyTable(['Selection', 'Events', 'Eff.(Skim) [%]', 'Rel.Eff. [%]'])
+        efftable.align = 'r'
         efftable.align['Selection'] = 'l'
-        efftable.align['Events'] = 'r'
-        efftable.align['Eff. (Skim) [%]'] = 'r'
-        efftable.align['Rel. Eff. [%]'] = 'r'
         skimnevents = self.cutflow.count(self.cutflow.getNames()[0])
 
         for i, name in enumerate(self.cutflow.getNames()):
@@ -174,6 +212,7 @@ def parse_command_line(argv):
     # line below is an example of a required argument
     parser.add_argument('inputFileList', type=str, help='List of input files (AC1B*.root)')
     parser.add_argument('outputFileName', type=str, help='Output file name')
+    parser.add_argument('pileupDir', type=str, help='Pileup files directory (usually AnalysisTool/data/pileup)')
     # line below is an example of an optional argument
     #parser.add_argument('--outputFileName', type=str, default='ana.out', help='Output file name')
 
@@ -182,6 +221,7 @@ def parse_command_line(argv):
 ## ___________________________________________________________
 def main(argv=None):
     if argv is None:
+        # sys.argv[0:] is the python script itself, sys.argv[1:] is the first argument
         argv = sys.argv[1:]
     args = parse_command_line(argv)
     return args
