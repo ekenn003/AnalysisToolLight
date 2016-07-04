550379d87ef2fc113ec7888b224cf4125ff93717
E Kennedy <ekennedy@cern.ch>
2016-07-04 13:23:32 +0200

    add batch dsets

---
+++ AnalysisTool/python/batch/datasets.py
@@ -0,0 +1,20 @@
+# Datasets ##############################
+datasets76X = {
+    'SingleMuon2015C' : { 'njobs' : 1 },
+    'SingleMuon2015D' : { 'njobs' : 12 },
+    'DYJetsToLL'  : { 'njobs' : 6 },
+    'TTJets'      : { 'njobs' : 2 },
+    'TTZToLLNuNu' : { 'njobs' : 1 },
+    'WWTo2L2Nu'   : { 'njobs' : 2 },
+    'WZTo2L2q'    : { 'njobs' : 8 },
+    'WZTo3LNu'    : { 'njobs' : 1 },
+    'ZZTo2L2Nu'   : { 'njobs' : 2 },
+    'ZZTo2L2q'    : { 'njobs' : 4 },
+    'ZZTo4L'      : { 'njobs' : 2 },
+}
+
+datasets80X = {
+    'SingleMuon2016B' : { 'njobs' : 10 },
+    'DYJetsToLL'  : { 'njobs' : 6 },
+}
+
