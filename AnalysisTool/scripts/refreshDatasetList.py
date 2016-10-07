0b1ef3bc51c4db4023fa958041d1fcff8062791f
E Kennedy <ekennedy@cern.ch>
2016-10-07 12:34:07 +0200

    refresh list but still print only

--- AnalysisTool/scripts/refreshDatasetList.py
+++ AnalysisTool/scripts/refreshDatasetList.py
@@ -1,8 +1,8 @@
 import os, sys
 import time
 
-t2 = 'T2_CH_CERN'
-#t2 = 'T2_US_UCSD'
+#t2 = 'T2_CH_CERN'
+t2 = 'T2_US_UCSD'
 
 era = 'oct16'
 
@@ -22,14 +22,7 @@ partialpath = 'cms/store/user/ekennedy/smh2mu/{0}/{1}'.format(version, era)
 
 
 # delete them if they exist
-for dset in ds:
-    outfile = '{0}/inputfiles_{1}.txt'.format(datadir, dset)
-    try:
-        #os.remove(outfile)
-        print 'os.remove({0})'.format(outfile)
-    except OSError:
-        pass
-for dset in ms:
+for dset in ds+ms:
     outfile = '{0}/inputfiles_{1}.txt'.format(datadir, dset)
     try:
         #os.remove(outfile)
@@ -41,12 +34,8 @@ for dset in ms:
 # set up commands to get new lists
 if (t2 == 'T2_CH_CERN'): 
 
-    for dset in ds:
-        fullpath = '{0}/data/{1}/*.*'.format(partialpath, dset)
-        command = 'eos ls /eos/{0} >> {1}/inputfiles_{2}.txt'.format(fullpath, datadir, dset)
-        commandlist += [command]
-    for dset in ms:
-        fullpath = '{0}/mc/{1}/*.*'.format(partialpath, dset)
+    for dset in ds+ms:
+        fullpath = '{0}/{2}/{1}/*.*'.format(partialpath, dset, 'data' if (dset in ds) else 'mc')
         command = 'eos ls /eos/{0} >> {1}/inputfiles_{2}.txt'.format(fullpath, datadir, dset)
         commandlist += [command]
 
@@ -58,19 +47,16 @@ elif (t2 == 'T2_US_UCSD'):
     print '    cmsenv'
     print
 
-    for dset in ds:
-        fullpath = '{0}/data/{1}/'.format(partialpath, dset)
+    for dset in ds+ms:
+        fullpath = '{0}/{2}/{1}/'.format(partialpath, dset, 'data' if (dset in ds) else 'mc')
         command = 'ssh ekennedy@uaf-4.t2.ucsd.edu "cd /hadoop/{0}; ls" >> {1}/inputfiles_{2}.txt'.format(fullpath, datadir, dset)
-        commandlist += [command]
-    for dset in ms:
-        fullpath = '{0}/mc/{1}/'.format(partialpath, dset)
-        command = 'ssh ekennedy@uaf-4.t2.ucsd.edu "cd /hadoop/{0}; ls *.*" >> {1}/inputfiles_{2}.txt'.format(fullpath, datadir, dset)
         commandlist += [command]
     
 else:
     raise ValueError('"{0}" is not a choice of T2 storage site'.format(t2))
 
 # execute commands
+print
 for c in commandlist:
     print c
     time.sleep(3)
