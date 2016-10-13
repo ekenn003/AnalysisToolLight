bbd69c4a288543ff23d4fd62bfd5a7df426e33e7
E Kennedy <ekennedy@cern.ch>
2016-10-13 13:32:58 +0200

    more 80x dsets

--- AnalysisTool/python/batch/datasets.py
+++ AnalysisTool/python/batch/datasets.py
@@ -1,20 +1,32 @@
 # Datasets ##############################
 datasets76X = {
+    # data
     'SingleMuon2015C' : { 'njobs' : 1 },
-    'SingleMuon2015D' : { 'njobs' : 12 },
-    'DYJetsToLL'  : { 'njobs' : 6 },
-    'TTJets'      : { 'njobs' : 2 },
-    'TTZToLLNuNu' : { 'njobs' : 1 },
-    'WWTo2L2Nu'   : { 'njobs' : 2 },
-    'WZTo2L2q'    : { 'njobs' : 8 },
-    'WZTo3LNu'    : { 'njobs' : 1 },
-    'ZZTo2L2Nu'   : { 'njobs' : 2 },
-    'ZZTo2L2q'    : { 'njobs' : 4 },
-    'ZZTo4L'      : { 'njobs' : 2 },
+    'SingleMuon2015D' : { 'njobs' : 20 },
+    # background
+    'DYJetsToLL'  : { 'njobs' : 12 },
+    'TTJets'      : { 'njobs' : 4 },
+    'TTZToLLNuNu' : { 'njobs' : 2 },
+    'WWTo2L2Nu'   : { 'njobs' : 4 },
+    'WZTo2L2Q'    : { 'njobs' : 16 },
+    'WZTo3LNu'    : { 'njobs' : 2 },
+    'ZZTo2L2Nu'   : { 'njobs' : 4 },
+    'ZZTo2L2Q'    : { 'njobs' : 8 },
+    'ZZTo4L'      : { 'njobs' : 4 },
 }
 
 datasets80X = {
-    'SingleMuon2016B' : { 'njobs' : 10 },
-    'DYJetsToLL'  : { 'njobs' : 6 },
+    # data
+    'SingleMuon2016B' : { 'njobs' : 20 },
+    'SingleMuon2016C' : { 'njobs' : 1 },
+    # signal
+    'GluGlu_HToMuMu' : { 'njobs' : 1 },
+    'VBF_HToMuMu'    : { 'njobs' : 1 },
+    # background
+    'DYJets'      : { 'njobs' : 10 },
+    'TTJets'      : { 'njobs' : 6 },
+    #'TTZJets'     : { 'njobs' : 5 },
+    #'TTWJets'     : { 'njobs' : 3 },
+    #'tZq_ll_4f'   : { 'njobs' : 2 },
 }
 
