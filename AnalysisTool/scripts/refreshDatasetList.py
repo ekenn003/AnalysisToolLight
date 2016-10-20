d4f3130a69584c61fbc9484208e250b904af648c
E Kennedy <ekennedy@cern.ch>
2016-10-20 14:44:27 +0200

    finalised refresh dataset list

--- AnalysisTool/scripts/refreshDatasetList.py
+++ AnalysisTool/scripts/refreshDatasetList.py
@@ -1,63 +1,92 @@
 import os, sys
 import time
+from shutil import move
+from tempfile import TemporaryFile
 
 #t2 = 'T2_CH_CERN'
 t2 = 'T2_US_UCSD'
 
 era = 'oct16'
 
-version = '80X'
+version = '76X'
+#version = '80X'
 
 
-ds = ['SingleMuon2016D', 'SingleMuon2016E']
+#ds = ['SingleMuon2016D', 'SingleMuon2016E']
+ds = []
 ms = ['DYJetsToLL', 'TTJets']
 
 
 
 
 # common
-commandlist = []
 datadir = '{0}/src/AnalysisToolLight/AnalysisTool/data/{1}'.format(os.environ['CMSSW_BASE'], version)
-partialpath = 'cms/store/user/ekennedy/smh2mu/{0}/{1}'.format(version, era)
+partialpath = 'store/user/ekennedy/smh2mu/{0}/{1}'.format(version, era)
 
 
-# delete them if they exist
-for dset in ds+ms:
-    outfile = '{0}/inputfiles_{1}.txt'.format(datadir, dset)
-    try:
-        #os.remove(outfile)
-        print 'os.remove({0})'.format(outfile)
-    except OSError:
-        pass
 
+# __________________________________________________________
+def main():
+    commandlist = []
 
-# set up commands to get new lists
-if (t2 == 'T2_CH_CERN'): 
-
+    # delete them if they exist
     for dset in ds+ms:
-        fullpath = '{0}/{2}/{1}/*.*'.format(partialpath, dset, 'data' if (dset in ds) else 'mc')
-        command = 'eos ls /eos/{0} >> {1}/inputfiles_{2}.txt'.format(fullpath, datadir, dset)
-        commandlist += [command]
-
-elif (t2 == 'T2_US_UCSD'):
-    #basecommand = 'gfal-ls srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=/hadoop/{0}/'.format(path)
-    print 'Make sure you have done the commands:'
-    print '    exec ssh-agent bash'
-    print '    ssh-add'
-    print '    cmsenv'
-    print
-
-    for dset in ds+ms:
-        fullpath = '{0}/{2}/{1}/'.format(partialpath, dset, 'data' if (dset in ds) else 'mc')
-        command = 'ssh ekennedy@uaf-4.t2.ucsd.edu "cd /hadoop/{0}; ls" >> {1}/inputfiles_{2}.txt'.format(fullpath, datadir, dset)
-        commandlist += [command]
+        outfile = '{0}/inputfiles_{1}.txt'.format(datadir, dset)
+        try:
+            os.remove(outfile)
+        except OSError:
+            print 'could not remove ' + outfile
+            pass
     
-else:
-    raise ValueError('"{0}" is not a choice of T2 storage site'.format(t2))
+    
+    # set up commands to get new lists
+    if (t2 == 'T2_CH_CERN'): 
+    
+        for dset in ds+ms:
+            fullpath = 'cms/{0}/{2}/{1}/*.*'.format(partialpath, dset, 'data' if (dset in ds) else 'mc')
+            command = 'eos ls /eos/{0} >> {1}/inputfiles_{2}.txt'.format(fullpath, datadir, dset)
+            commandlist += [command]
+    
+    elif (t2 == 'T2_US_UCSD'):
+        #basecommand = 'gfal-ls srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=/hadoop/{0}/'.format(path)
+        print 'Make sure you have done the commands:'
+        print '    exec ssh-agent bash'
+        print '    ssh-add'
+        print '    cmsenv'
+        print
+    
+        for dset in ds+ms:
+            fullpath = 'cms/{0}/{2}/{1}/'.format(partialpath, dset, 'data' if (dset in ds) else 'mc')
+            command = 'ssh ekennedy@uaf-4.t2.ucsd.edu "cd /hadoop/{0}; ls" >> {1}/inputfiles_{2}.txt'.format(fullpath, datadir, dset)
+            commandlist += [command]
+        
+    else:
+        raise ValueError('"{0}" is not a choice of T2 storage site'.format(t2))
+    
+    # execute commands
+    for c in commandlist:
+        #print c
+        os.system(c)
 
-# execute commands
-print
-for c in commandlist:
-    print c
-    time.sleep(3)
+    # prepend with correct path
+    print '\nCreated the following files:'
+    for dset in ds+ms:
+        f = '{0}/inputfiles_{1}.txt'.format(datadir, dset)
+        f_ = TemporaryFile()
+        with open(f) as fin:
+            with open(f_.name, 'w') as fout:
+                for line in fin:
+                    fout.write(''.join([t2, '/', partialpath, '/', line]))
+        move(f_.name, f)
+        print '    ' + f
+
+
+
+
+
+# __________________________________________________________
+try:
+    main()
+except KeyboardInterrupt:
+    print
 
