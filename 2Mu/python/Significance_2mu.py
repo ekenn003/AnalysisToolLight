7508c3f6c4bca52c190a3bd9dec0f6b4fa000e6d
E Kennedy <ekennedy@cern.ch>
2017-06-12 23:15:23 +0200

    new significance

--- 2Mu/python/Significance_2mu.py
+++ 2Mu/python/Significance_2mu.py
@@ -33,30 +33,134 @@ class Ana2Mu(AnalysisBase):
 
 
 
-        basehist2D = ROOT.TH2D('hDiMuInvMass', 'hDiMuInvMass', 3, 110., 140., 3, 110., 140.)
+        #basehist2D = ROOT.TH2D('hDiMuInvMass', 'hDiMuInvMass', 3, 110., 140., 3, 110., 140.)
 
         self.histograms_scan = {}
 
         # book step 1 histograms
-        for i in xrange(500,801,10):
-            self.histograms_scan[('hDiMuInvMass_VBF_dijetMassAbove'
-                +str(i)+'_deta2p5')] = basehist2D.Clone(
-                    'hDiMuInvMass_dijetmassAbove'+str(i)+'_deta2p5')
-            self.histograms_scan[('hDiMuInvMass_VBF_dijetMassAbove'
-                +str(i)+'_deta3p0')] = basehist2D.Clone(
-                    'hDiMuInvMass_dijetmassAbove'+str(i)+'_deta3p0')
-            self.histograms_scan[('hDiMuInvMass_VBF_dijetMassAbove'
-                +str(i)+'_deta3p5')] = basehist2D.Clone(
-                    'hDiMuInvMass_dijetmassAbove'+str(i)+'_deta3p5')
-            self.histograms_scan[('hDiMuInvMass_VBF_dijetMassAbove'
-                +str(i)+'_deta4p0')] = basehist2D.Clone(
-                    'hDiMuInvMass_dijetmassAbove'+str(i)+'_deta4p0')
-            self.histograms_scan[('hDiMuInvMass_VBF_dijetMassAbove'
-                +str(i)+'_deta4p5')] = basehist2D.Clone(
-                    'hDiMuInvMass_dijetmassAbove'+str(i)+'_deta4p5')
-            self.histograms_scan[('hDiMuInvMass_VBF_dijetMassAbove'
-                +str(i)+'_deta5p0')] = basehist2D.Clone(
-                    'hDiMuInvMass_dijetmassAbove'+str(i)+'_deta5p0')
+        # met
+        # dijet mass
+        # dijet deta
+        # dimu pt
+
+
+        met_hist = TH1F('hMET', 'hMET', 500, 0., 500.)
+        met_hist.GetXaxis().SetTitle('E_{T miss}[GeV/c]')
+        met_hist.GetYaxis().SetTitle('Candidates/2.0[GeV/c]')
+
+        dijetmass_hist = TH1F('hDiJetInvMass', 'hDiJetInvMass', 2000, 0, 1000)
+        dijetmass_hist.GetXaxis().SetTitle('M_{j^{+}j^{-}} [GeV/c^{2}]')
+        dijetmass_hist.GetYaxis().SetTitle('Candidates/0.5[GeV/c^{2}]')
+
+        dijetdeta_hist = TH1F('hDiJetDeltaEta', 'hDiJetDeltaEta',  132, 0., 6.6)
+        dijetdeta_hist.GetXaxis().SetTitle('|#Delta #eta_{j^{+} - j^{-}}|')
+        dijetdeta_hist.GetYaxis().SetTitle('Candidates/0.1')
+
+        dimupt_hist = TH1F('hDiMuPt', 'hDiMuPt', 500, 0., 1000.)
+        dimupt_hist.GetXaxis().SetTitle('p_{T #mu^{+}#mu^{-}}[GeV/c]')
+        dimupt_hist.GetYaxis().SetTitle('Candidates/2.0[GeV]')
+
+        # MET
+        for dijetmassvbf in range(2):
+            for dijetmass in range(2):
+                for dijetdeta in range(2):
+                    for dimuptggf in range(2):
+                        for dimupt in range(2):
+                            # dijetmassvbf is always higher than dijetmass
+                            if dijetmassvbf and not dijetmass: continue
+                            # dimuptggf is always higher than dimupt
+                            if dimuptggf and not dimupt: continue
+                            hname = ('hMET_dijetmassvbf{0}_dijetmass{1}_dijetdeta{2}_'
+                                     'dimuptggf{3}_dimupt{4}').format(dijetmassvbf,
+                                         dijetmass, dijetdeta, dimuptggf, dimupt)
+                            print hname
+                            self.histograms_scan[hname] = met_hist.Clone(hname)
+
+        # dijetmass vbf
+        for met in range(2):
+            for dijetmass in range(2):
+                for dijetdeta in range(2):
+                    for dimuptggf in range(2):
+                        for dimupt in range(2):
+                            # dimuptggf is always higher than dimupt
+                            if dimuptggf and not dimupt: continue
+                            hname = ('hDiJetInvMassVBF_met{0}_dijetmass{1}_dijetdeta{2}_'
+                                     'dimuptggf{3}_dimupt{4}').format(met,
+                                         dijetmass, dijetdeta, dimuptggf, dimupt)
+                            print hname
+                            self.histograms_scan[hname] = dijetmass_hist.Clone(hname)
+
+        # dijetmass ggf
+        for met in range(2):
+            for dijetmassvbf in range(2):
+                for dijetdeta in range(2):
+                    for dimuptggf in range(2):
+                        for dimupt in range(2):
+                            # dimuptggf is always higher than dimupt
+                            if dimuptggf and not dimupt: continue
+                            hname = ('hDiJetInvMass_met{0}_dijetmassvbf{1}_dijetdeta{2}_'
+                                     'dimuptggf{3}_dimupt{4}').format(met,
+                                         dijetmassvbf, dijetdeta, dimuptggf, dimupt)
+                            print hname
+                            self.histograms_scan[hname] = dijetmass_hist.Clone(hname)
+
+
+
+
+        # dijet d eta
+        for met in range(2):
+            for dijetmassvbf in range(2):
+                for dijetmass in range(2):
+                    for dimuptggf in range(2):
+                        for dimupt in range(2):
+                            # dijetmassvbf is always higher than dijetmass
+                            if dijetmassvbf and not dijetmass: continue
+                            # dimuptggf is always higher than dimupt
+                            if dimuptggf and not dimupt: continue
+                            hname = ('hDiJetDeltaEta_met{0}_dijetmassvbf{1}_dijetmass{2}_'
+                                     'dimuptggf{3}_dimupt{4}').format(met,
+                                         dijetmassvbf, dijetmass, dimuptggf, dimupt)
+                            print hname
+                            self.histograms_scan[hname] = dijetdeta_hist.Clone(hname)
+
+
+        # dimupt ggf
+        for met in range(2):
+            for dijetmassvbf in range(2):
+                for dijetmass in range(2):
+                    for dijetdeta in range(2):
+                        for dimupt in range(2):
+                            # dijetmassvbf is always higher than dijetmass
+                            if dijetmassvbf and not dijetmass: continue
+                            hname = ('hDiMuPtGGF_met{0}_dijetmassvbf{1}_dijetmass{2}_'
+                                     'dijetdeta{3}_dimupt{4}').format(met,
+                                         dijetmassvbf, dijetmass, dijetdeta, dimupt)
+                            print hname
+                            self.histograms_scan[hname] = dimupt_hist.Clone(hname)
+
+
+        # dimupt
+        for met in range(2):
+            for dijetmassvbf in range(2):
+                for dijetmass in range(2):
+                    for dijetdeta in range(2):
+                        for dimuptggf in range(2):
+                            # dijetmassvbf is always higher than dijetmass
+                            if dijetmassvbf and not dijetmass: continue
+                            hname = ('hDiMuPt_met{0}_dijetmassvbf{1}_dijetmass{2}_'
+                                     'dijetdeta{3}_dimuptggf{4}').format(met,
+                                         dijetmassvbf, dijetmass, dijetdeta, dimuptggf)
+                            print hname
+                            self.histograms_scan[hname] = dimupt_hist.Clone(hname)
+
+
+
+
+
+
+
+
+
 
         # add it to the extra histogram map
         self.extra_histogram_map['scan'] = self.histograms_scan
@@ -91,41 +195,63 @@ class Ana2Mu(AnalysisBase):
 
         ##########################################################
         #                                                        #
-        # Determine category                                     #
         #                                                        #
         ##########################################################
         thisdimupt = dimuonobj.Pt()
-        this_cat = -1
+        thismet = self.met.et()
 
         # VBF tagged
-        if (len(self.good_jets) > 1 
+        twojetcondition = (len(self.good_jets) > 1 
             and self.good_jets[0].pt() > cuts['VBF_lead_jet_pt']
-            and self.good_jets[1].pt() > cuts['VBF_sublead_jet_pt']
-            and self.met.et() < cuts['VBF_met']):
+            and self.good_jets[1].pt() > cuts['VBF_sublead_jet_pt'])
 
 
-            thisdijetmass = (self.good_jets[0].p4() + self.good_jets[1].p4()).M()
-            thisdijetdeta = abs(self.good_jets[0].eta() - self.good_jets[1].eta())
+        thisdijetmass = (self.good_jets[0].p4() + self.good_jets[1].p4()).M() if twojetcondition else 0.
+        thisdijetdeta = abs(self.good_jets[0].eta() - self.good_jets[1].eta()) if twojetcondition else 0.
+
+        b_met = int(thismet > 40.)
+        b_dijetmassvbf = int(thisdijetmass > 650.)
+        b_dijetmass = int(thisdijetmass > 250.)
+        b_dijetdeta = int(thisdijetdeta > 3.5)
+        b_dimuptggf = int(thisdimupt > 50.)
+        b_dimupt = int(thisdimupt > 10.)
+
+        thishMET = ('hMET_dijetmassvbf{0}_dijetmass{1}_dijetdeta{2}_'
+                    'dimuptggf{3}_dimupt{4}').format(b_dijetmassvbf, b_dijetmass, 
+                        b_dijetdeta, b_dimuptggf, b_dimupt)
+
+
+        thishDiJetInvMassVBF = ('hDiJetInvMassVBF_met{0}_dijetmass{1}_dijetdeta{2}_'
+                 'dimuptggf{3}_dimupt{4}').format(b_met, b_dijetmass,
+                     b_dijetdeta, b_dimuptggf, b_dimupt)
+
+
+        thishDiJetInvMass = ('hDiJetInvMass_met{0}_dijetmassvbf{1}_dijetdeta{2}_'
+                 'dimuptggf{3}_dimupt{4}').format(b_met, b_dijetmassvbf,
+                     b_dijetdeta, b_dimuptggf, b_dimupt)
+
+        thishDiJetDeltaEta = ('hDiJetDeltaEta_met{0}_dijetmassvbf{1}_dijetmass{2}_'
+                 'dimuptggf{3}_dimupt{4}').format(b_met, b_dijetmassvbf,
+                     b_dijetmass, b_dimuptggf, b_dimupt)
+
+        thishDiMuPtGGF = ('hDiMuPtGGF_met{0}_dijetmassvbf{1}_dijetmass{2}_'
+                 'dijetdeta{3}_dimupt{4}').format(b_met, b_dijetmassvbf, b_dijetmass,
+                     b_dijetdeta, b_dimupt)
+
+        thishDiMuPt = ('hDiMuPt_met{0}_dijetmassvbf{1}_dijetmass{2}_'
+                 'dijetdeta{3}_dimuptggf{4}').format(b_met, b_dijetmassvbf, b_dijetmass,
+                     b_dijetdeta, b_dimupt)
 
 
 
-            for i in xrange(500,801,10):
-                if thisdijetmass > i: 
-                    if thisdijetdeta > 2.5:
-                        self.histograms_scan['hDiMuInvMass_VBF_dijetMassAbove'+str(i)+'_deta2p5'].Fill(mytInvMass, mytInvMass, eventweight)
-                    if thisdijetdeta > 3.0:
-                        self.histograms_scan['hDiMuInvMass_VBF_dijetMassAbove'+str(i)+'_deta3p0'].Fill(mytInvMass, mytInvMass, eventweight)
-                    if thisdijetdeta > 3.5:
-                        self.histograms_scan['hDiMuInvMass_VBF_dijetMassAbove'+str(i)+'_deta3p5'].Fill(mytInvMass, mytInvMass, eventweight)
-                    if thisdijetdeta > 4.0:
-                        self.histograms_scan['hDiMuInvMass_VBF_dijetMassAbove'+str(i)+'_deta4p0'].Fill(mytInvMass, mytInvMass, eventweight)
-                    if thisdijetdeta > 4.5:
-                        self.histograms_scan['hDiMuInvMass_VBF_dijetMassAbove'+str(i)+'_deta4p5'].Fill(mytInvMass, mytInvMass, eventweight)
-                    if thisdijetdeta > 5.0:
-                        self.histograms_scan['hDiMuInvMass_VBF_dijetMassAbove'+str(i)+'_deta5p0'].Fill(mytInvMass, mytInvMass, eventweight)
 
 
-
+        self.histograms_scan[thishMET].Fill(thismet, eventweight)
+        self.histograms_scan[thishDiJetInvMassVBF].Fill(thisdijetmass, eventweight)
+        self.histograms_scan[thishDiJetInvMass].Fill(thisdijetmass, eventweight)
+        self.histograms_scan[thishDiJetDeltaEta].Fill(thisdijetdeta, eventweight)
+        self.histograms_scan[thishDiMuPtGGF].Fill(thisdimupt, eventweight)
+        self.histograms_scan[thishDiMuPt].Fill(thisdimupt, eventweight)
 
 #            # VBFTight
 #            if (thisdijetmass > cuts['VBF_dijet_mass'] 
